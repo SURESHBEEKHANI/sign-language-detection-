98094c5acf31a66af74a5926bc41946f9b726afd
sureshbeekhani <sureshbeekhani26@gmail.com>
2024-11-06 12:59:16 -0800

    data ingestion

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-from signLanguage.logger import logging
-from signLanguage.exception import SignException
-import sys  # Import sys to pass it to SignException
+from signLanguage.pipeline.training_pipeline import TrainPipeline
 
-# Logging a message
-logging.info("Welcome to my application")
 
-try:
-    a = 7 / "8"  # No quotes around 8; it should be an integer for division
-except Exception as e:
-    raise SignException(e, sys)
+obj=TrainPipeline()
+obj.run_pipeline()
